f61e15d58246be6c414d3e6b672362ac4cb5cad9
byo.spoon <byo.spoon@gmail.com>
2011-09-20 22:01:28 +0200

    Create initial GWT project

---
+++ gwt/ZKCrypto/src/com/google/code/zkcrypto/webtest/client/GreetingService.java
@@ -0,0 +1,12 @@
+package com.google.code.zkcrypto.webtest.client;
+
+import com.google.gwt.user.client.rpc.RemoteService;
+import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
+
+/**
+ * The client side stub for the RPC service.
+ */
+@RemoteServiceRelativePath("greet")
+public interface GreetingService extends RemoteService {
+	String greetServer(String name) throws IllegalArgumentException;
+}
